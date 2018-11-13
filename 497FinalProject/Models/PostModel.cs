7873efb0e8b503a6b20e723c72e4fe6f56217934
makayladorroh <mbdorroh@crimson.ua.edu>
2018-11-13 14:46:28 -0600

    Added the post model

---
+++ 497FinalProject/Models/PostModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _497FinalProject.Models
+{
+    public class PostModel
+    {
+        public string Subject { get; set; }
+        public string PostBody { get; set; }
+        public string PostUserName { get; set; }
+        public string PostUserType { get; set; }
+        public DateTime TimePost { get; set; }
+        public int Approval { get; set; }
+        public int Disapproval { get; set; }
+        public bool isSolution { get; set; }
+        public int ThreadID { get; set; }
+        public string Comment { get; set; }
+    }
+}
