8b4a7338d10862ee44ab629b265ef081a499615f
Jason Frederick <jsfrederick@crimson.ua.edu>
2018-11-15 14:12:26 -0600

    Create new model

---
+++ 497FinalProject/Models/ThreadModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _497FinalProject.Models
+{
+    public class PublicClassThreadModel
+    {
+        public string ThreadName { get; set; }
+        public string ThreadCategory { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateOfLastPost { get; set; }
+        public int NoOfPosts { get; set; }
+        public int ThreadID { get; set; }
+    }
+}
