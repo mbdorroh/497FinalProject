7da6ed8a6fcf2312794070a0b89734b468af5203
Jason Frederick <jsfrederick@crimson.ua.edu>
2018-11-29 13:38:19 -0600

    Revert "Revert "Fixed minor typo""
    This reverts commit 6622e4d1014ea779d22e8e95002cdc02eb6403e0.

--- 497FinalProject/Controllers/ManageController.cs
+++ 497FinalProject/Controllers/ManageController.cs
@@ -67,7 +67,7 @@ namespace _497FinalProject.Controllers
             var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
             Dictionary<string, string> roles = RoleManager.Roles.ToDictionary(r => r.Id, r => r.Name);
 
-            var roleId = roles.Where(r => r.Value == "professor").FirstOrDefault().Key;
+            var roleId = roles.Where(r => r.Value == "Professor").FirstOrDefault().Key;
             var professorUsers = users.Where(u => u.Roles.Any(r => r.RoleId == roleId)).ToList();
 
             roleId = roles.Where(r => r.Value == "admin").FirstOrDefault().Key;
