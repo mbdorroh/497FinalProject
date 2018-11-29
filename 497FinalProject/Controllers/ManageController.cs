e39710ad92860ec0fef01fcae0d97c04065bf24d
makayladorroh <mbdorroh@crimson.ua.edu>
2018-11-29 16:37:21 -0600

    Revert "Revert "Changes""
    This reverts commit 85d8c72b530080192405cdda9c7eea81fa881fcb.

--- 497FinalProject/Controllers/ManageController.cs
+++ 497FinalProject/Controllers/ManageController.cs
@@ -260,10 +260,10 @@ namespace _497FinalProject.Controllers
             Dictionary<string, string> roles = RoleManager.Roles.ToDictionary(r => r.Id, r => r.Name);
 
             bool isInProfessor = false;
-            bool isInAdmin = false;
+            //bool isInAdmin = false;
             bool isInStudent = false;
             bool isInTA = false;
-            bool isInIT = false;
+            //bool isInIT = false;
 
             var roleId = roles.Where(r => r.Value == "admin").FirstOrDefault().Key;
             if (user.Roles.Any(r => r.RoleId == roleId))
